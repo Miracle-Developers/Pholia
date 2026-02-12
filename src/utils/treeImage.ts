14f412e41621a0658d01e475943802f7170fb9f0
kazu3jp <kazu3jp.purin@outlook.jp>
2026-02-12 09:20:48 +0900

    change: 展示用に木の変化のために必要な葉の枚数を変更

--- src/utils/treeImage.ts
+++ src/utils/treeImage.ts
@@ -1,10 +1,10 @@
 import { ImageSourcePropType } from "react-native";
 
 export const getTreeImageSource = (leafCount: number): ImageSourcePropType => {
-    if (leafCount < 5) {
+    if (leafCount < 1) {
         return require("@/../assets/tree(sick).png");
     }
-    if (leafCount < 20) {
+    if (leafCount < 5) {
         return require("@/../assets/tree(normal).png");
     }
     return require("@/../assets/tree(fun).png");
